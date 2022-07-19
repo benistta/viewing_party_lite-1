5ac4c9ecfe5cae85cfc0e15845dad7b83e95d868
Becky Nisttahuz <benistta@hotmail.com>
2022-07-18 21:41:24 -0600

    Feat:add method create login and login user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+
 class UsersController < ApplicationController
   def new
     @user = User.new
@@ -13,16 +14,30 @@ class UsersController < ApplicationController
     if new_user.save
       redirect_to user_path(new_user.id), notice: "Welcome, #{new_user.name}!"
     else
-      redirect_to register_path, alert: "Cannot register, missing or repeated information"
-   end 
+      redirect_to register_path, alert: new_user.errors.full_messages
+   end
   end
 
   def discover
     @user = User.find(params[:user_id])
   end
 
+  def login
+  end
+
+  def login_user
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      redirect_to user_path(user.id)
+      flash[:success] = "Welcome back, #{user.email}"
+    else
+      flash[:error] = 'Invalid Credentials'
+      render :login
+    end
+  end
+
 private
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
